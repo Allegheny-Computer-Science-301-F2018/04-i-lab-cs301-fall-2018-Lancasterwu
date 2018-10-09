38751c8020b2f8c7a1e191f28037411242241276
Juncheng Wu <wuj@aldenv103.allegheny.edu>
2018-10-09 16:20:09 -0400

    finished first 4

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -1,4 +1,4 @@
-# Name:
+# Name:Lancaster
 # Date:
 
 # Run the below only if the library is not already installed.
@@ -11,7 +11,14 @@ data(us_contagious_diseases)
 
 #Question 1.
 
+dat <- filter(us_contagious_diseases, disease == "Measles", state != "Alaska" & state != "Hawaii")
+per100000rate <- dat$count * 100000 / dat$population * dat$weeks_reporting / 52
+dat$per100000rate <- per100000rate
+
 #Question 2.
+
+library(ggplot2)
+ggplot(data = datc) + geom_point(mapping = aes(x = year, y = count/population)) + geom_vline(xintercept = 1965)
 
 #Question 3.
 
