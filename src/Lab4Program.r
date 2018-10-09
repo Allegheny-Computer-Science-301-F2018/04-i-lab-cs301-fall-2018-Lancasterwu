58af019498b39cd114866d59effbeac5c9ed9694
Juncheng Wu <wuj@aldenv103.allegheny.edu>
2018-10-09 16:24:19 -0400

    import a data base

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -1,5 +1,5 @@
 # Name:Lancaster
-# Date:
+# Date:10/9/2018
 
 # Run the below only if the library is not already installed.
 # install.packages(dslabs)
@@ -11,22 +11,48 @@ data(us_contagious_diseases)
 
 #Question 1.
 
+## create the object dat, use filter to find all Measles variables without state == Alaska and Hawaii.
 dat <- filter(us_contagious_diseases, disease == "Measles", state != "Alaska" & state != "Hawaii")
+## set the function to calculate per100000rate
 per100000rate <- dat$count * 100000 / dat$population * dat$weeks_reporting / 52
+## add the function as the new column
 dat$per100000rate <- per100000rate
 
 #Question 2.
 
 library(ggplot2)
+## create the object datc, use filter to find data only with state == California
+datc = filter(dat, state == "California")
+## polt the point graph, add the vericle line at x == 1965
 ggplot(data = datc) + geom_point(mapping = aes(x = year, y = count/population)) + geom_vline(xintercept = 1965)
 
 #Question 3.
 
+dat_caliFocus <- filter(us_contagious_diseases, state == "California")
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1950] <- "1950’s"
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1960] <- "1960’s"
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1970] <- "1970’s"
+C_sqrt = sqrt(dat_caliFocus$count)
+library(rcompanion)
+plotNormalHistogram(C_sqrt)
+
 #Question 4.
+
+dat_copy <- us_contagious_diseases
+dat_copy$yearBlock[dat_copy$year == 1950] <- "1950’s"
+dat_copy$yearBlock[dat_copy$year == 1960] <- "1960’s"
+dat_copy$yearBlock[dat_copy$year == 1970] <- "1970’s"
+count_sqrt = sqrt(dat_copy$count)
+plotNormalHistogram(count_sqrt)
 
 #Question 5.
 
+
+
 #Question 6.
+
+## find the data from https://github.com/cran/SCCS
+## Name of the file: autdat
 
 
 
