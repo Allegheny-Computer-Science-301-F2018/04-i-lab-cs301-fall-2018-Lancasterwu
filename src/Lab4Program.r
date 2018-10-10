b0654d4b9547ed7fe22ba499f89b67fd372686cd
Juncheng Wu <wuj@aldenv101.allegheny.edu>
2018-10-09 20:42:21 -0400

    finished all

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -21,13 +21,25 @@ dat$per100000rate <- per100000rate
 #Question 2.
 
 library(ggplot2)
-## create the object datc, use filter to find data only with state == California
+## create the object datc, use filter to find data only with state == California from dat
 datc = filter(dat, state == "California")
 ## polt the point graph, add the vericle line at x == 1965
 ggplot(data = datc) + geom_point(mapping = aes(x = year, y = count/population)) + geom_vline(xintercept = 1965)
 
 #Question 3.
 
+## Solution1:
+## create the object with only state == California from us_contagious_diseases
+dat_caliFocus <- filter(us_contagious_diseases, state == "California")
+## block the years besides 1950, 1960, 1970
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1950] <- "1950’s"
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1960] <- "1960’s"
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1970] <- "1970’s"
+## plot the graph, set the width to 0.5
+ggplot(data = dat_caliFocus) + geom_bar(mapping = aes(x = state,y = count^(1/2), fill = yearBlock), width = 0.5, position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+## year 1970's has the lowest rate
+
+## Solution2:
 install.packages("rcompanion")
 dat_caliFocus <- filter(us_contagious_diseases, state == "California")
 dat_caliFocus$yearBlock[dat_caliFocus$year == 1950] <- "1950’s"
@@ -39,6 +51,18 @@ plotNormalHistogram(C_sqrt)
 
 #Question 4.
 
+## SOlution1:
+## create a copy of us_contagious_diseases
+dat_copy <- us_contagious_diseases
+## block all the years besides 1950, 1960, 1970
+dat_copy$yearBlock[dat_copy$year == 1950] <- "1950’s"
+dat_copy$yearBlock[dat_copy$year == 1960] <- "1960’s"
+dat_copy$yearBlock[dat_copy$year == 1970] <- "1970’s"
+## plot the graph
+ggplot(data = dat_copy) + geom_bar(mapping = aes(x = state,y = count^(1/2), fill = yearBlock), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
+## It shows all states
+
+## Solution2:
 dat_copy <- us_contagious_diseases
 dat_copy$yearBlock[dat_copy$year == 1950] <- "1950’s"
 dat_copy$yearBlock[dat_copy$year == 1960] <- "1960’s"
@@ -48,7 +72,15 @@ plotNormalHistogram(count_sqrt)
 
 #Question 5.
 
-
+## First two steps are extactly the same
+## create a copy of us_contagious_diseases
+dat_copy <- us_contagious_diseases
+## block all the years besides 1950, 1960, 1970
+dat_copy$yearBlock[dat_copy$year == 1950] <- "1950’s"
+dat_copy$yearBlock[dat_copy$year == 1960] <- "1960’s"
+dat_copy$yearBlock[dat_copy$year == 1970] <- "1970’s"
+## plot the graph, use geom_tile, set x = state, y = year, fill = rate, add the grey color edge
+ggplot(data = dat_copy) + geom_tile(mapping = aes(x = state, y = year, fill = count^(1/2)),colour = "grey50", position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
 
 #Question 6.
 
